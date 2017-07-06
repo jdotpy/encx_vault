1c6ceeb170696af759c8a118ce79f762ef0c3ea6
KJ <jdotpy@users.noreply.github.com>
2017-07-05 23:51:43 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='encx_vault',
-    version='0.1',
+    version='0.2.0',
     description='Extension to Encx that adds a client for the encx vault server',
     author='KJ',
     author_email='<redacted>',
